61895109399b4fb1793f3c0602578272fa362b9c
Eduardo L. Costa <dev.eduardo.lc@gmail.com>
2022-11-15 18:44:12 -0300

    Estrutura inicial do contexto de autenticacao

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,18 +4,22 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@
 import { THEME } from './src/styles/theme'
 import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/SignIn';
+import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      { !fontsLoaded ? <Loading /> : <SignIn /> }
+      <AuthContextProvider>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+
+        { !fontsLoaded ? <Loading /> : <SignIn /> }
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
